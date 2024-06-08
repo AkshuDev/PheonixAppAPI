38442432efc005d72f18a41b241bb4b354f31ac9
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-06-08 20:37:51 +0530

    Delete PheonixApp_API_BaseDIR/PheonixAppAPI directory

--- PheonixApp_API_BaseDIR/PheonixAppAPI/main.py
+++
@@ -1,9 +0,0 @@
-import os
-from PheonixAppAPI.pheonixapp.files import PSSbridge
-
-class INITIALIZE:
-    def __init__(self, LoginOrSignup:bool=False, email:str="", username:str="", password:str=""):
-        if LoginOrSignup:
-            PSSbridge.API(False, email, username, password).LoginOrSignup()
-        else:
-            PSSbridge.API(False, email, username, password).No_LoginOrSignUp()
