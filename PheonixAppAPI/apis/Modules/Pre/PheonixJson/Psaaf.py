8c3e70157f8776244108072a73a85409d459b18a
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-08-02 21:34:34 +0530

    Update Psaaf.py

--- PheonixAppAPI/apis/Modules/Pre/PheonixJson/Psaaf.py
+++ PheonixAppAPI/apis/Modules/Pre/PheonixJson/Psaaf.py
@@ -32,6 +32,85 @@ class PheonixStudiosAccessFile:
 
         return content
 
+    def _str_to_dict(self, data:str) -> dict:
+        content:dict = {}
+
+        data_str = data.splitlines()
+        skip = False
+        gather = ""
+        testv = ""
+        inside = False
+        current_class = ""
+
+        content["Classes"] = {}
+        content["Important Variables"] = {}
+        content["Data"] = {}
+
+        for i, v in enumerate(data_str):
+            if not skip and not inside:
+                if v.startswith('##') or v.startswith("#$"):
+                    if v.startswith("##"):
+                        content[v] = ""
+                        continue
+                    else:
+                        skip = True
+                        testv = v
+                        gather += v
+                elif v.startswith("@"):
+                    current_class = v.replace(" ", "").replace("->", "").replace("{", "").replace("@", "")
+                    content["Classes"][v.replace(" ", "").replace("->", "").replace("{", "")] = current_class
+                    content["Data"][current_class] = {}
+                    inside = True
+                    continue
+                else:
+                    if "->" in v:
+                        v = v.replace(" ", "").split("->")
+                        if v[0].islower and not "|" in v[0]:
+                            content["Data"][current_class][v[0]] = v[1].replace(",", "")
+                        elif v[0].isupper() and "|" in v[0]:
+                            content["Data"][current_class][v[0].replace("|", "")] = v[1].replace(",", "")
+                            content["Important Variables"][v[0].replace("|", "")] = v[1].replace(",", "")
+
+            if inside:
+                if v.startswith('##') or v.startswith("#$"):
+                    if v.startswith("##"):
+                        content["Data"][current_class][v] = ""
+                        continue
+                    else:
+                        skip = True
+                        testv = v
+                        gather += v
+                elif v.endswith("}"):
+                    inside = False
+                    continue
+                else:
+                    if "->" in v:
+                        v = v.replace(" ", "").split("->")
+                        if v[0].islower and not "|" in v[0]:
+                            content["Data"][current_class][v[0]] = v[1].replace(",", "")
+                        elif v[0].isupper() and "|" in v[0]:
+                            content["Data"][current_class][v[0].replace("|", "")] = v[1].replace(",", "")
+                            content["Important Variables"][v[0].replace("|", "")] = v[1].replace(",", "")
+
+            if skip and not inside:
+                if v.endswith("#$"):
+                    skip = False
+                    if not testv == v:
+                        gather += v
+                    content[gather] = ""
+                    gather = ""
+                    continue
+            elif skip and inside:
+                if v.endswith("#$"):
+                    skip = False
+                    if not testv == v:
+                        gather += v
+                    content["Data"][current_class][gather] = ""
+                    gather = ""
+                    continue
+
+        return content
+
     def read(self, name_of_file:str) -> dict:
         """Reads the paaf file
 
@@ -42,7 +121,14 @@ class PheonixStudiosAccessFile:
             dict: The output
         """
         data = ""
-        # with open(os.path.join(self.path, name_of_file+".paaf"))
+        with open(os.path.join(self.path, name_of_file+".paaf")) as f:
+            f.seek(0)
+            data = f.read()
+
+        #process data
+        data = self._str_to_dict(data)
+
+        return data
 
     def create(self, name_of_file:str, metadata:dict={"##This is the metadata class": ""}, access:dict={"##This is the access class": ""}, other_content:dict={"##Other content (OC) class": ""}) -> None:
         """This function creates a new .psaaf file.
